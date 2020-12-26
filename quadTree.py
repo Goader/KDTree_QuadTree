9494579c07291a14c1b2761bbc24c27942be17c0
Darnok00 <56363711+Darnok00@users.noreply.github.com>
2020-12-26 19:20:22 +0100

    Update quadTree.py

--- quadTree.py
+++ quadTree.py
@@ -1,49 +1,69 @@
+
+
 class Point:
     def __init__(self,point):
        self.x = point[0]
        self.y = point[1]
 
-class Rectnagle:
+    def follows(self, other):
+        return(self.x >= other.x and self.y >= other.y)
+
+    def precedes(self, other):
+        return(self.x <= other.x and self.y <= other.y)
+
+    def __str__(self):
+        return f'({self.x}, {self.y})'
+    def __repr__(self):
+        return self.__str__()
+
+
+class Rectangle:
     def __init__(self,left_down_corner,right_upper_corner):
         self.x_down = left_down_corner[0]
         self.y_down = left_down_corner[1]
         self.x_upper = right_upper_corner[0]
         self.y_upper = right_upper_corner[1]
 
+    def classification(self, other):
+        if (2*other.x_down - other.x_upper > 2*self.x_down + self.x_upper or
+        2*other.x_down + other.x_upper > 2*self.x_down - self.x_upper or
+        2*other.y_down - other.y_upper > 2*self.y_down + self.y_upper or
+        2*other.y_down + other.y_upper > 2*self.y_down - self.y_upper):
+            return True
+        else:
+            return False
+
     def contains(self,point):
             return (point.x >= self.x_down and point.x <=self.x_upper and point.y >= self.y_down and point.y <= self.y_upper )
 
-class QuadTree:
-    def __init__(self,boundary,capacity,intersection_points):
+
+class Node:
+    def __init__(self,boundary,capacity):
         self.boundary = boundary
         self.capacity = capacity
         self.points = []
         self.divided = False
-        self.intersection_points = intersection_points
+
+
     def subdivide(self):
         x_down = self.boundary.x_down
         y_down = self.boundary.y_down
         height = self.boundary.y_upper - self.boundary.y_down
         width= self.boundary.x_upper - self.boundary.x_down
-        right_upper_rectangle = Rectnagle([x_down+width/2,y_down+height/2],[x_down+width,y_down+height])
-        left_upper_rectangle = Rectnagle([x_down,y_down+height/2],[x_down+width/2,y_down+height])
-        right_down_rectangle = Rectnagle([x_down+width/2,y_down],[x_down+width,y_down+height/2])
-        left_down_rectangle = Rectnagle([x_down,y_down],[x_down+width/2,y_down+height/2])
-        self.right_upper = QuadTree(right_upper_rectangle,self.capacity,self.intersection_points)
-        self.left_upper = QuadTree(left_upper_rectangle, self.capacity,self.intersection_points)
-        self.left_down = QuadTree(left_down_rectangle,self.capacity,self.intersection_points)
-        self.right_down = QuadTree(right_down_rectangle,self.capacity,self.intersection_points)
-        self.intersection_points.append([x_down+width/2,y_down+height/2])
-        self.intersection_points.append([x_down+width/2,y_down])
-        self.intersection_points.append([x_down+width/2,y_down+height])
-        self.intersection_points.append([x_down,y_down+height/2])
-        self.intersection_points.append([x_down+width,y_down+height/2])
+        right_upper_rectangle = Rectangle([x_down + width / 2, y_down + height / 2], [x_down + width, y_down + height])
+        left_upper_rectangle = Rectangle([x_down, y_down + height / 2], [x_down + width / 2, y_down + height])
+        right_down_rectangle = Rectangle([x_down + width / 2, y_down], [x_down + width, y_down + height / 2])
+        left_down_rectangle = Rectangle([x_down, y_down], [x_down + width / 2, y_down + height / 2])
+        self.right_upper = Node(right_upper_rectangle, self.capacity)
+        self.left_upper = Node(left_upper_rectangle, self.capacity)
+        self.left_down = Node(left_down_rectangle, self.capacity)
+        self.right_down = Node(right_down_rectangle, self.capacity)
+
     def insert(self,point):
         if(self.boundary.contains(point)==False):
             return
         if (len(self.points)<self.capacity):
             self.points.append(point)
-            return self.intersection_points
         else:
             if (self.divided == False):
                 self.subdivide()
@@ -52,13 +72,48 @@ class QuadTree:
             self.right_down.insert(point)
             self.left_upper.insert(point)
             self.left_down.insert(point)
+    def points_in_rec(self,rect,points_to_find):
 
-def find_all_intersections_point(rectangular,points):
-    intersections_points =[]
-    quad_tree = QuadTree(rectangular,1,[])
+        if(rect.classification(self.boundary)==False):
+            return
+        else:
+            for p in self.points:
+                if rect.contains(p):
+                    points_to_find.append(p)
+            if (self.divided):
+                self.left_down.points_in_rec(rect,points_to_find)
+                self.left_upper.points_in_rec(rect,points_to_find)
+                self.right_down.points_in_rec(rect,points_to_find)
+                self.right_upper.points_in_rec(rect,points_to_find)
+            return points_to_find
+
+class Quadtree:
+    def __init__(self,node):
+        self.node = node
+
+    def insert_all(self,points):
+        for i in range (len(points)):
+            self.node.insert(points[i])
+    def find_points_in_rec(self,rectangle):
+        points=[]
+        return self.node.points_in_rec(rectangle,points)
+
+def create_rectangular(points):
+    right_upper_corner = Point([float('inf'),float('inf')])
+    left_down_corner = Point([float('-inf'),float('-inf')])
     for i in range (len(points)):
-        intersections_points.append(quad_tree.insert(points[i]))
-    return (intersections_points[0])
+        if(points[i].precedes(right_upper_corner)):
+            right_upper_corner=points[i]
+        if(points[i].follows(left_down_corner)):
+            left_down_corner=points[i]
+    left_down_corner = [left_down_corner.x,left_down_corner.y]
+    right_upper_corner = [right_upper_corner.x,right_upper_corner.y]
+    return Rectangle(left_down_corner,right_upper_corner)
 
-rectangular = Rectnagle([0,0],[20,20])
-print(find_all_intersections_point(rectangular,[Point([0,2]),Point([10,5]),Point([7,7]),Point([7,8])]))
+
+rectangular = Rectangle([0,0],[20,20])
+rectangular1 = Rectangle([2,2],[8,8])
+node = Node(rectangular,1)
+quadtree = Quadtree(node)
+quadtree.insert_all([Point([8,8]),Point([6,5]),Point([6,6]),Point([9,7])])
+print(quadtree.find_points_in_rec(rectangular1))
